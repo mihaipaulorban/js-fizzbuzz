534fb80e7e9dd64e90f1e69f43c4c7dae7372801
mihaipaulorban <mihaipaulorban@gmail.com>
2023-11-08 16:43:49 +0100

    feat: lista in html

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-      console.log("Fizz");
-    } else if (i % 5 === 0) {
-      console.log("Buzz");
-    } else {
-      console.log(i);
+ // Funzione che generara la lista di FizzBuzz e la agggiunge nell'html
+ function fizzBuzz() {
+    const fizzList = document.getElementById("listafizz");
+
+    for (let i = 1; i <= 100; i++) {
+      let listItem = document.createElement("li");
+
+      if (i % 3 === 0 && i % 5 === 0) {
+        listItem.textContent = "FizzBuzz";
+      } else if (i % 3 === 0) {
+        listItem.textContent = "Fizz";
+      } else if (i % 5 === 0) {
+        listItem.textContent = "Buzz";
+      } else {
+        listItem.textContent = i;
+      }
+
+      fizzList.appendChild(listItem);
     }
   }
-  
+
+  fizzBuzz();
