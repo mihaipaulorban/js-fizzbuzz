a05c724a6b080762fa9a20ba6d2482e46fde3bce
mihaipaulorban <mihaipaulorban@gmail.com>
2023-11-08 17:31:20 +0100

    feat: stili di bootstrap, responsive e fix output

---
+++ README.md
@@ -0,0 +1,14 @@
+# FizzBuzzDOM
+
+1-Creo un evento che mi genera una lista
+2-Iniziamo da 1 e continuiamo fino a 100.
+3-Per ogni numero, verifichiamo se è multiplo di 3, 5 o entrambi.
+4-A seconda delle condizioni, inseriamo nel quadrato uno dei seguenti contenuti:
+-Se il numero è multiplo di 3, inseriamo "Fizz".
+-Se il numero è multiplo di 5, inseriamo "Buzz".
+-Se il numero è multiplo sia di 3 che di 5, inseriamo "FizzBuzz".
+-Se il numero non è multiplo di né 3 né 5, inseriamo il numero stesso.
+
+In breve, JavaScript esamina ogni numero da 1 a 100 e decide cosa inserire nei quadrati in base alle regole del "FizzBuzz".
+
+Aggiungo poi stili di Bootstrap e responsive.
--- script.js
+++ script.js
@@ -1,29 +1,36 @@
- // Funzione che generara la lista di FizzBuzz e la agggiunge nell'html
- function fizzBuzz() {
-    const outputList = document.getElementById("output");
-
-    for (let i = 1; i <= 100; i++) {
-      let listItem = document.createElement("li");
-      listItem.classList.add("col-2", "mb-2", "p-2", "text-center");
-      let bgColorClass = "";
-      if (i % 3 === 0 && i % 5 === 0) {
-        listItem.textContent = "FizzBuzz";
-        bgColorClass = "bg-danger";
-      } else if (i % 3 === 0) {
-        listItem.textContent = "Fizz";
-        bgColorClass = "bg-success";
-      } else if (i % 5 === 0) {
-        listItem.textContent = "Buzz";
-        bgColorClass = "bg-warning";
-      } else {
-        listItem.textContent = i;
+document.addEventListener("DOMContentLoaded", function () {
+    // Funzione che genera la lista FizzBuzz e la aggiunge all'HTML
+    function fizzBuzz() {
+      const outputList = document.getElementById("output");
+  
+      for (let i = 1; i <= 100; i++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        let output = "";
+  
+        if (i % 3 === 0 && i % 5 === 0) {
+          square.classList.add("fizzbuzz");
+          output = "FizzBuzz";
+        } else if (i % 3 === 0) {
+          square.classList.add("fizz");
+          output = "Fizz";
+        } else if (i % 5 === 0) {
+          square.classList.add("buzz");
+          output = "Buzz";
+        } else {
+          square.classList.add("normal");
+          output = i;
+        }
+  
+        square.textContent = output;
+  
+        outputList.appendChild(square);
+  
+        // Aggiunge il contenuto alla console
+        console.log(output);
       }
-
-      if (bgColorClass) {
-        listItem.classList.add(bgColorClass);
-      }
-      outputList.appendChild(listItem);
     }
-  }
-
-  fizzBuzz();
+  
+    fizzBuzz();
+  });
+  
