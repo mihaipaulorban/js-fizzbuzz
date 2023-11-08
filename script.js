2127f506f30065b73ea1ce16c10b030804ac4109
mihaipaulorban <mihaipaulorban@gmail.com>
2023-11-08 16:56:23 +0100

    feat: griglia con bootstrap

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
  // Funzione che generara la lista di FizzBuzz e la agggiunge nell'html
  function fizzBuzz() {
-    const fizzList = document.getElementById("listafizz");
+    const outputList = document.getElementById("output");
 
     for (let i = 1; i <= 100; i++) {
       let listItem = document.createElement("li");
-
+      listItem.classList.add("col-2", "mb-2", "p-2", "text-center");
+      let bgColorClass = "";
       if (i % 3 === 0 && i % 5 === 0) {
         listItem.textContent = "FizzBuzz";
+        bgColorClass = "bg-danger";
       } else if (i % 3 === 0) {
         listItem.textContent = "Fizz";
+        bgColorClass = "bg-success";
       } else if (i % 5 === 0) {
         listItem.textContent = "Buzz";
+        bgColorClass = "bg-warning";
       } else {
         listItem.textContent = i;
       }
 
-      fizzList.appendChild(listItem);
+      if (bgColorClass) {
+        listItem.classList.add(bgColorClass);
+      }
+      outputList.appendChild(listItem);
     }
   }
 
